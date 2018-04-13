aa6b5d3c702975738c0c92d41f7c837cc155b8f0
AntoineVaxelaire <35782544+AntoineVaxelaire@users.noreply.github.com>
2018-04-13 15:59:19 +0200

    Update case.cpp

--- Projet_2048/case.cpp
+++ Projet_2048/case.cpp
@@ -1,6 +1,18 @@
 #include "case.h"
 
-Case::Case(QObject *parent) : QObject(parent)
+Case::Case(int ligne, int colonne, Grille grille, QObject *parent) : QObject(parent)
 {
-
+    this->ligne=ligne;
+    this->colonne=colonne;
+    this->grille=grille;
+    fCase=0;
+    cptChanged();
 }
+
+QString Case::readCase()
+{
+    fCase= grille [ligne][colonne];
+   return QString::number(fCase);
+}
+
+

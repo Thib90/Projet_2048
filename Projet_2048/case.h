6fdf03e5b9d9fca6d207f141d9be5bbc031314d9
AntoineVaxelaire <35782544+AntoineVaxelaire@users.noreply.github.com>
2018-04-13 15:58:57 +0200

    Update case header

--- Projet_2048/case.h
+++ Projet_2048/case.h
@@ -2,17 +2,32 @@
 #define CASE_H
 
 #include <QObject>
+#include "grille.h"
+
 
 class Case : public QObject
 {
     Q_OBJECT
 public:
-    explicit Case(QObject *parent = nullptr);
-    void FusionCase (); //Permet de gérer l'addition de 2 cases.
+    Case(int ligne, int colonne, Grille grille, QObject *parent = 0);
+
+    Q_PROPERTY(QString CaseQML READ readCase NOTIFY caseChanged);
+    Q_PROPERTY(QString ColorQML READ readColor NOTIFY ColorChanged)
+
+
+    QString readCase();
+    QString readColor();
 
 signals:
+    void cptChanged();
 
 public slots:
+
+private:
+    int fCase;
+    int ligne;
+    int colonne;
+    Grille grille;
 };
 
 #endif // CASE_H
